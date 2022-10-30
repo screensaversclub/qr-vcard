3d08891dcdfa17c4759aa5aea8cb561022436e9f
siahgoodwork <83395882+siahgoodwork@users.noreply.github.com>
2022-10-30 23:43:37 +0800

    add dist files

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.QRVCard = void 0;
+const qr_code_and_vcard_1 = require("qr-code-and-vcard");
+const QRVCard = { QrCode: qr_code_and_vcard_1.QrCode };
+exports.QRVCard = QRVCard;
